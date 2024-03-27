ba293a5fdee82488f8656a66fb23837bbe71a7f4
diayaya1999@gmail.com <diayaya1999@gmail.com>
2024-03-27 14:39:00 +0000

    rejet de certains fichiers

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,4 @@
 import Dashboard from "components/Dashboard/Dashboard"
-import Feeds from "components/Feeds/Feeds"
-import ProjectTables from "components/ProjectTable/ProjectTables";
 import React from "react"
 
 function HomePage() {
